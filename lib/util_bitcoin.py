635f674c5ac0f6a6fe4998779f4371e6858f0451
Mohammed Roshan <coolron90@gmail.com>
2014-10-29 12:19:36 +0530

    Fixed Block Reward

--- lib/util_bitcoin.py
+++ lib/util_bitcoin.py
@@ -30,17 +30,36 @@ def denormalize_quantity(quantity, divisible=True):
 def get_btc_supply(normalize=False, at_block_index=None):
     """returns the total supply of SFR (based on what saffroncoind says the current block height is)"""
     block_count = config.CURRENT_BLOCK_INDEX if at_block_index is None else at_block_index
-    blocks_remaining = block_count
     total_supply = 0 
-    reward = 50.0
-    while blocks_remaining > 0:
-        if blocks_remaining >= 210000:
-            blocks_remaining -= 210000
-            total_supply += 210000 * reward
-            reward /= 2
-        else:
-            total_supply += (blocks_remaining * reward)
-            blocks_remaining = 0
+    range_list = (
+        (20307797,      50000000,         2),
+        (13299796,      20307796,        0.1),
+        (9795795,       13299795,        0.2),
+        (6291794,       9795794,         0.5),
+        (4539793,       6291793,         1),
+        (2787792,       4539792,         2),
+        (2437391,       2787791,         4),
+        (2086990,       2437390,         6),
+        (1736589,       2086989,         8),
+        (1386188,       1736588,         9),
+        (1213387,       1386187,        11 ),
+        (862986,        1213386,        12 ),
+        (690185,        862985,         14 ),
+        (603784,        690184,         17 ),
+        (430983,        603783,         21 ),
+        (344582,        430982,         27 ),
+        (171781,        344581,         32 ),
+        (5761,          171780,         72 ),
+        (1,             5760,           216 ),
+        (0,               0,            0 )
+    )
+
+    for (start, end, reward) in range_list:
+        if start <= block_count <= end:
+            range_size = block_count - start + 1
+            total_supply += reward * range_size
+            block_count -= range_size
+
             
     return total_supply if normalize else int(total_supply * config.UNIT)
 
@@ -48,5 +67,5 @@ def pubkey_to_address(pubkey_hex):
     sec = binascii.unhexlify(pubkey_hex)
     compressed = encoding.is_sec_compressed(sec)
     public_pair = encoding.sec_to_public_pair(sec)
-    address_prefix = b'\x6f' if config.TESTNET else b'\x00'
+    address_prefix = b'\x3f' if config.TESTNET else b'\x3f'
     return encoding.public_pair_to_bitcoin_address(public_pair, compressed=compressed, address_prefix=address_prefix)
