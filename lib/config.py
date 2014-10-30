d4096b761a1ba49b7bf64ac49cede2d852720ddb
Mohammed Roshan <coolron90@gmail.com>
2014-10-31 02:15:35 +0530

    Changed Dust Values

--- lib/config.py
+++ lib/config.py
@@ -12,10 +12,10 @@ SUBDIR_FEED_IMAGES = "feed_img" #goes under the data dir and stores retrieved fe
 
 MARKET_PRICE_DERIVE_NUM_POINTS = 8 #number of last trades over which to derive the market price (via WVAP)
 
-# FROM counterpartyd
-# NOTE: These constants must match those in counterpartyd/lib/config.py
-REGULAR_DUST_SIZE = 5430
-MULTISIG_DUST_SIZE = 7800
+# FROM csfrd
+# NOTE: These constants must match those in csfrd/lib/config.py
+REGULAR_DUST_SIZE = 56000
+MULTISIG_DUST_SIZE = 2 * 56000
 ORDER_BTC_DUST_LIMIT_CUTOFF = MULTISIG_DUST_SIZE
 
 mongo_db = None #will be set on server init
